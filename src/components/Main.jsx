2e1f22ba1b6cc19c772fe5a6d15c38ae6769e7fd
Ekene Gerald <geraldekene0@gmail.com>
2023-07-03 01:49:45 +0100

    my second commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import apibtn from "../images/icon-dice.jpg";
+import divider from "../images/pattern-divider-desktop.jpg";
+import dividermb from "../images/pattern-divider-mobile.jpg"
 
 const Main = () => {
   const [data, setData] = useState("");
@@ -8,9 +10,8 @@ const Main = () => {
   const handleFetch = async () => {
     try {
       const response = await fetch(url);
-      const data = await response.json();
-      setData(data);
-      console.log("Data:", data["slip"]["advice"]);
+      const responseData = await response.json();
+      setData(responseData.slip);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -19,20 +20,29 @@ const Main = () => {
   useEffect(() => {}, [url]);
 
   const submit = (e) => {
-    handleFetch();
     e.preventDefault();
-    setUrl("https://api.adviceslip.com/advice");
+    handleFetch();
   };
 
   return (
     <div className="main">
-      <h1>Hello</h1>
-      {data && <p>{data["slip"]["advice"]}</p>}
+      <div className="body">
+      <h5 className="title">ADVICE #{data.id}</h5><br />
+      {data && (
+        <>
+          <p className="advice">"{data.advice}"</p> <br />
+        </>
+      )}
+      <img className="divider" src={divider} alt="" /> <br />
+      <img className="divider2 d-md-none" src={dividermb} alt="" /> <br />
+      </div>
+      <div className="body-ii">
       <form onClick={submit}>
-        <button type="submit">
+        <button className="submit" type="submit">
           <img src={apibtn} alt="API button" />
         </button>
       </form>
+      </div>
     </div>
   );
 };
